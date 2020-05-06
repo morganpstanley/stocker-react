ee0abc475c618fb1aef49093498d241116d8acbc
morganpstanley <morganpstanley@gmail.com>
2020-05-06 12:08:57 -0600

    add ADJUST_PRICE case to stockReducer

--- src/reducers/stockReducer.js
+++ src/reducers/stockReducer.js
@@ -1,15 +1,32 @@
 const stocksReducer = (state = { stocks: [], loading: false }, action) => {
+  console.log('STATE: ', state, 'ACTION: ', action)
     switch(action.type) {
+
       case 'LOADING_STOCKS':
         return {
           ...state,
           stocks: [...state.stocks],
         }
+
       case 'ADD_STOCK':
         return {
           ...state,
-          stocks: action.stocks,
+          stocks: [...state.stocks, action.stocks]
         }
+
+      case 'ADJUST_PRICE':
+        return {
+          ...state,
+          stocks: state.stocks.map(stock => {
+            if (stock.n === action.stock) {
+              return {
+                ...stock, c: action.price
+              }
+            }
+              return stock
+          })
+        }
+        
       default:
         return state;
     }
