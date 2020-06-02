9bacb7fc7afe68bc14a41bfeac149c9d55badd7f
morganpstanley <morganpstanley@gmail.com>
2020-06-02 00:29:43 -0600

    add dashboard component to App, add ownedStocks and watchedStocks methods

--- src/App.js
+++ src/App.js
@@ -3,22 +3,37 @@ import { connect } from "react-redux";
 
 import Header from './components/Header'
 import StockContainer from './components/StockContainer';
-import AddStockForm from './components/AddStockForm'
+import Dashboard from './components/Dashboard'
 
 import './App.css';
 
 class App extends Component {
 
+  ownedStocks = () => {
+    return this.props.stocks.filter(stock => stock.amountOfShares > 0)
+  }
+
+  watchedStocks = () => {
+    return this.props.stocks.filter(stock => stock.amountOfShares === undefined)
+  }
+
 
   render() {
     return (
-      <div>
+      <div className="app">
         <Header />
-        <AddStockForm />
-        <StockContainer />
+        <Dashboard stocks={this.ownedStocks()}/>
+        <StockContainer stocks={this.ownedStocks()}/>
+        <StockContainer stocks={this.watchedStocks()}/>
       </div>
     );
   }
 }
 
-export default connect()(App);
+const mapStateToProps = state => {
+  return {
+    stocks: state.stocks
+  }
+}
+
+export default connect(mapStateToProps)(App);
