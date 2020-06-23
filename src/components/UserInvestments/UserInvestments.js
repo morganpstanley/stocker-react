6f84bd3d9f45d9e133fa3ae13319fb43de5cb796
morganpstanley <morganpstanley@gmail.com>
2020-06-22 20:12:09 -0600

    add percentChangeOverall to userInvestments

--- src/components/UserInvestments/UserInvestments.js
+++ src/components/UserInvestments/UserInvestments.js
@@ -11,16 +11,21 @@ const UserInvestments = ({stocks}) => {
         return a + (b.amountOfShares * b.pc)
     }, 0)
 
-    const percentChange = (currentValue > 0) ? ((currentValue - previousCloseValue) * 100 / previousCloseValue).toFixed(2) : '0.00'   
+    const originalValue = stocks.reduce((a, b) => {
+        return a + (b.costPerShare * b.amountOfShares)
+    }, 0)
+
+    const percentChangeToday = (currentValue > 0) ? ((currentValue - previousCloseValue) * 100 / previousCloseValue).toFixed(2) : '0.00'   
+    const percentChangeOverall = (currentValue > 0) ? ((currentValue - originalValue) * 100 / originalValue).toFixed(2) : '0.00'
 
     return (
        <div className="dashboard-part" id="user-investments">
            <h3 id="portfolio-value-title"> PORTFOLIO VALUE </h3>
            <span id="portfolio-value-value">${currentValue.toLocaleString('en-US', {minimumFractionDigits: 2, maximumFractionDigits: 2})}</span>
            <span id="percent-change-today-title"> Change Today: </span>
-           <span id="percent-change-today-value">{percentChange}%</span>
+           <span id="percent-change-today-value">{percentChangeToday}%</span>
            <span id="percent-change-overall-title"> Change Overall: </span>
-           <span id="percent-change-overall-value"> 10.00% </span>
+           <span id="percent-change-overall-value"> {percentChangeOverall}% </span>
        </div>
     )
 }
