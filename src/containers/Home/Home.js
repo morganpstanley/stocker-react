ba6459d8e9674d8255d4439dcb7278d4f1e1c7d6
morganpstanley <morganpstanley@gmail.com>
2020-06-26 21:20:10 -0600

    add user to state and then add stocks that only relate to that user

--- src/containers/Home/Home.js
+++ src/containers/Home/Home.js
@@ -5,23 +5,43 @@ import { fetchStock } from '../../actions/fetchStock'
 import Header from '../../components/Header/Header'
 import StockContainer from '../StockContainer/StockContainer';
 import Dashboard from '../Dashboard/Dashboard'
+import axios from 'axios'
+
 
 import './Home.css';
 
 class Home extends Component {
 
+  state = {
+    user: '',
+    user_id: ''
+  }
+
   componentDidMount = () => {
-      return fetch(`http://localhost:3000/stocks`)
+
+    axios.get('http://localhost:3000/logged_in', {withCredentials: true})
+    .then(response => {
+      if (response.data.logged_in) {
+       this.setState({
+         user: response.data.user.username,
+         user_id: response.data.user.id
+       })
+      } 
+    }).then(
+      fetch(`http://localhost:3000/stocks`)
       .then(response => {
-          console.log(response)
           return response.json();
       })
-      .then(json => {          
-          console.log(json) 
+      .then(json => {  
+        console.log(json)        
           json.forEach(element => {
-              this.props.fetchStock(element.ticker_symbol, element.name, element.purchase_amount, element.purchase_price)         
+            console.log('element: ', element, 'user_id: ', this.state)
+            if (element.user_id == this.state.user_id) {
+              this.props.fetchStock(element.ticker_symbol, element.name, element.purchase_amount, element.purchase_price, element.id)
+            }         
           });
-      });
+      })
+    )
   }
 
   ownedStocks = () => {
@@ -32,11 +52,21 @@ class Home extends Component {
     return this.props.stocks.filter(stock => stock.amountOfShares === null)
   }
 
+  handleLogoutClick = () => {
+    console.log('going to log out')
+    axios.delete('http://localhost:3000/logout', {withCredentials: true})
+    .then(response => {
+        console.log(response)
+        this.props.history.push('/login')
+        this.props.handleLogout()
+    })
+    .catch(error => console.log(error))
+  }
 
   render() {
     return (
       <div className="app">
-        <Header link='/login' title="LOG OUT" />
+        <Header handleLogoutClick={this.handleLogoutClick} title="LOG OUT" />
         <Dashboard stocks={this.ownedStocks()}/>
         <StockContainer stocks={this.ownedStocks()} stockType="OWNED" />
         <StockContainer stocks={this.watchedStocks()} stockType="WATCHED" />
@@ -47,13 +77,13 @@ class Home extends Component {
 
 const mapStateToProps = state => {
   return {
-    stocks: state.stocks
+    stocks: state.stocksReducer.stocks
   }
 }
 
 const mapDispatchToProps = dispatch => {
   return({
-      fetchStock: (tickerSymbol, companyName, amountOfShares, costPerShare) => dispatch(fetchStock(tickerSymbol, companyName, amountOfShares, costPerShare))
+      fetchStock: (tickerSymbol, companyName, amountOfShares, costPerShare, id) => dispatch(fetchStock(tickerSymbol, companyName, amountOfShares, costPerShare, id))
   })
 }
 
