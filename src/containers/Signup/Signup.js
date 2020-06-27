650900aa277a9bfcd44e0d0643a7b75729960f91
morganpstanley <morganpstanley@gmail.com>
2020-06-26 21:17:53 -0600

    add signup page functionality

--- src/containers/Signup/Signup.js
+++ src/containers/Signup/Signup.js
@@ -6,80 +6,117 @@ import './Signup.css'
 
 class Signup extends Component {
 
-    state = {
-        username: '',
-        password: '',
-        passwordConfirmation: ''
+  state = {
+      username: '',
+      password: '',
+      password_confirmation: ''
+  }
+
+    // handleSubmit = event => {
+    //     event.preventDefault();
+    //     axios.post("http://localhost:3000/users", {
+    //         user: {
+    //             username: this.state.username,
+    //             password: this.state.password,
+    //             password_confirmation: this.state.passwordConfirmation
+    //         }
+    //     })
+    //     .then(response => {
+    //         console.log('sweet!', response)
+    //     })
+    //     .catch(error => {
+    //         console.log(error)
+    //     })
+    // }
+
+  componentDidMount = () => {
+    return this.props.loggedInStatus ? this.redirect() : null
+  }
+
+  handleSubmit = (event) => {
+    console.log('im going try')
+    event.preventDefault()
+    const {username, password_confirmation, password} = this.state
+
+    let user = {
+      username: username,
+      password_confirmation: password_confirmation,
+      password: password
     }
-
-    handleSubmit = event => {
-        event.preventDefault();
-        axios.post("http://localhost:3000/users", {
-            user: {
-                username: this.state.username,
-                password: this.state.password,
-                password_confirmation: this.state.passwordConfirmation
-            }
-        })
-        .then(response => {
-            console.log('sweet!', response)
-        })
-        .catch(error => {
-            console.log(error)
-        })
-    }
-
-    handleChange = event => {
-        console.log(event.target.value)
-
+      
+    axios.post("http://localhost:3000/users", {user}, {withCredentials: true})
+    .then(response => {
+      console.log('deeper')
+      console.log('here:', response.data)
+      if (response.data.logged_in) {
+        console.log('wo')
+        this.props.handleLogin(response.data)
+        this.redirect()
+      } else {
         this.setState({
-            [event.target.name]: event.target.value
+          errors: response.data.errors
         })
+      }
+    })
+    .catch(error => console.log('api errors:', error))
+    };
 
-        console.log('state: ', this.state)
-    }
+  redirect = () => {
+    console.log('leaving...')
+    this.props.history.push('/')
+  }
+
+  handleChange = event => {
+    console.log(event.target.value)
+
+    this.setState({
+      [event.target.name]: event.target.value
+    })
+
+    console.log('state: ', this.state)
+  }
 
 
-    render() {
-        return(
-            <div id="signup-page">
-                <Header />
-                <div id="signup-body">
-                    <h1>WELCOME TO STOCKER.</h1>
-                    <p>A simple platform to keep track of your stock portfolio. </p>
-                    <p>Ready to join? Enter a username and password below. </p>
-                    <form onSubmit={this.handleSubmit}>
-                        <input 
-                            type="text" 
-                            placeholder="username" 
-                            name="username" 
-                            value={this.state.username} 
-                            onChange={this.handleChange} 
-                            required
-                        /> <br />
-                        <input 
-                            type="text" 
-                            placeholder="password" 
-                            name="password" 
-                            value={this.state.password} 
-                            onChange={this.handleChange} 
-                            required
-                        /> <br />
-                        <input 
-                            type="text" 
-                            placeholder="confirm password" 
-                            name="passwordConfirmation" 
-                            value={this.state.passwordConfirmation} 
-                            onChange={this.handleChange} 
-                            required
-                        /> <br />
-                        <button>Sign Up</button>
-                    </form>
-                    <p>Already a member? <Link to="/login">Sign in here.</Link></p>  
-                </div>
-            </div>
-        )
-    }
+  render() {
+    return(
+      <div id="signup-page">
+        <Header />
+        <div id="signup-body">
+          <h1>WELCOME TO STOCKER.</h1>
+          <p>A simple platform to keep track of your stock portfolio. </p>
+          <p>Ready to join? Enter a username and password below. </p>
+          <form onSubmit={this.handleSubmit}>
+            <input 
+              type="text" 
+              placeholder="username" 
+              name="username" 
+              value={this.state.username} 
+              onChange={this.handleChange} 
+              required
+            /> <br />
+            <input 
+              type="text" 
+              placeholder="password" 
+              name="password" 
+              value={this.state.password} 
+              onChange={this.handleChange} 
+              required
+            /> <br />
+            <input 
+              type="text" 
+              placeholder="confirm password" 
+              name="password_confirmation" 
+              value={this.state.password_confirmation} 
+              onChange={this.handleChange} 
+              required
+              /> <br />
+              <button>Sign Up</button>
+          </form>
+          <p>Already a member? <Link to="/login">Sign in here.</Link></p>  
+        </div>
+      </div>
+    )
+  }
 }
 
 export default Signup
