152172faf0a9e9525d59c0fd7ee87dcfb7da5899
morganpstanley <morganpstanley@gmail.com>
2020-06-26 21:18:56 -0600

    add handleLogin, handleLogout, and loginStatus functions to App class

--- src/containers/App/App.js
+++ src/containers/App/App.js
@@ -1,23 +1,77 @@
 import React, { Component } from 'react';
 import { connect } from "react-redux";
 import { fetchStock } from '../../actions/fetchStock'
+import axios from 'axios'
 
 import Login from '../Login/Login'
 import Signup from '../Signup/Signup'
 import Home from '../Home/Home'
 import {BrowserRouter as Router, Switch, Route} from "react-router-dom";
+import history from '../../history'
 
 class App extends Component {
+
+  state = { 
+      isLoggedIn: false,
+      user: {}
+  };
+
+  componentDidMount() {
+    this.loginStatus()
+  }
+
+  handleLogin = ({data}) => {
+    this.setState({
+      isLoggedIn: true,
+      user: data.user
+    })
+    setTimeout(() => console.log('this.state', this.state), 2000)
+  }
+
+  handleLogout = () => {
+    console.log('logging out')
+    this.setState({
+    isLoggedIn: false,
+    user: {}
+    })
+  }
+
+  loginStatus = () => {
+    axios.get('http://localhost:3000/logged_in', {withCredentials: true})
+    .then(response => {
+      if (response.data.logged_in) {
+        this.handleLogin(response)
+      } else {
+        this.handleLogout()
+      }
+    })
+    .catch(error => console.log('api errors:', error))
+  }
 
   render() {
     return (
       <div>
-        <Router>
-            <Switch>
-                <Route exact path='/' component={Home}/>
-                <Route exact path='/login' component={Login}/>
-                <Route exact path='/signup' component={Signup}/>
-            </Switch>
+        <Router history={history}>
+          <Switch>
+            <Route 
+              exact path='/' 
+              render={props => (
+              <Home {...props} user={this.state.user} loggedInStatus={this.state.isLoggedIn} handleLogout={this.handleLogout}/>
+              )}
+            />
+            <Route 
+              exact path='/login' 
+              render={props => (
+              <Login {...props} handleLogin={this.handleLogin} loggedInStatus={this.state.isLoggedIn}/>
+              )}
+            />
+            <Route 
+              exact path='/signup' 
+              render={props => (
+              <Signup {...props} handleLogin={this.handleLogin} loggedInStatus={this.state.isLoggedIn}/>
+              )}
+            />
+          </Switch>
         </Router>
       </div>
     );
@@ -26,7 +80,7 @@ class App extends Component {
 
 const mapStateToProps = state => {
   return {
-    stocks: state.stocks
+    stocks: state.stocksReducer.stocks
   }
 }
 
