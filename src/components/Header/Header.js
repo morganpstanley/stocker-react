1f68b5a273ed9d777339e52c3934f67c0bba908f
morganpstanley <morganpstanley@gmail.com>
2020-06-29 17:07:28 -0600

    change header from classless lemming to classy cosmopolite

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,20 +1,42 @@
-import React from "react"
+import React, { Component } from "react"
+import { connect } from 'react-redux'
 import './Header.css'
-import {Link} from "react-router-dom";
+import axios from 'axios'
+import { withRouter } from 'react-router-dom'
+import { logoutUser } from '../../actions/logoutUser'
 
-const Header = ({link, title}) => {
-    return (
-        <h1 id="header">
-            <span id='logo'>
-                <span id="logo-stock">STOCK</span><span id="logo-er">ER</span>
-            </span>
-            <span id="nav">
-                    <nav>
-                        <Link to={link} id="log-in-link">{title}</Link>
-                    </nav>
-            </span>
-        </h1>
-    )
+class Header extends Component {
+
+    handleClick = () => {
+    console.log('going to log out')
+    return axios.delete('http://localhost:3000/logout', {withCredentials: true})
+    .then(response => {
+        this.props.history.push('/login')
+        this.props.logoutUser()
+    })
+    .catch(error => console.log(error))
+    }
+
+    render() {
+        return (
+            <h1 id="header">
+                <span id='logo'>
+                    <span id="logo-stock">STOCK</span><span id="logo-er">ER</span>
+                </span>
+                <span id="nav">
+                        <button onClick={this.handleClick}>
+                            Logout
+                        </button>
+                </span>
+            </h1>
+        )
+    }
 }
 
-export default Header
+const mapDispatchToProps = dispatch => {
+    return({
+        logoutUser: () => dispatch(logoutUser())
+    })
+  }
+
+export default withRouter(connect(null, mapDispatchToProps)(Header))
