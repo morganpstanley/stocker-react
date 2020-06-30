fac971ce8801800b8a311fabd7b70e45e61a0d37
morganpstanley <morganpstanley@gmail.com>
2020-06-29 18:05:20 -0600

    move logout button handler to header component, and move fetching stocks to App componenet

--- src/containers/Home/Home.js
+++ src/containers/Home/Home.js
@@ -1,48 +1,15 @@
 import React, { Component } from 'react';
 import { connect } from "react-redux";
 import { fetchStock } from '../../actions/fetchStock'
+import { logoutUser} from '../../actions/logoutUser'
 
 import Header from '../../components/Header/Header'
 import StockContainer from '../StockContainer/StockContainer';
 import Dashboard from '../Dashboard/Dashboard'
-import axios from 'axios'
-
 
 import './Home.css';
 
 class Home extends Component {
-
-  state = {
-    user: '',
-    user_id: ''
-  }
-
-  componentDidMount = () => {
-
-    axios.get('http://localhost:3000/logged_in', {withCredentials: true})
-    .then(response => {
-      if (response.data.logged_in) {
-       this.setState({
-         user: response.data.user.username,
-         user_id: response.data.user.id
-       })
-      } 
-    }).then(
-      fetch(`http://localhost:3000/stocks`)
-      .then(response => {
-          return response.json();
-      })
-      .then(json => {  
-        console.log(json)        
-          json.forEach(element => {
-            console.log('element: ', element, 'user_id: ', this.state)
-            if (element.user_id == this.state.user_id) {
-              this.props.fetchStock(element.ticker_symbol, element.name, element.purchase_amount, element.purchase_price, element.id)
-            }         
-          });
-      })
-    )
-  }
 
   ownedStocks = () => {
     return this.props.stocks.filter(stock => stock.amountOfShares > 0)
@@ -52,21 +19,10 @@ class Home extends Component {
     return this.props.stocks.filter(stock => stock.amountOfShares === null)
   }
 
-  handleLogoutClick = () => {
-    console.log('going to log out')
-    axios.delete('http://localhost:3000/logout', {withCredentials: true})
-    .then(response => {
-        console.log(response)
-        this.props.history.push('/login')
-        this.props.handleLogout()
-    })
-    .catch(error => console.log(error))
-  }
-
   render() {
     return (
       <div className="app">
-        <Header handleLogoutClick={this.handleLogoutClick} title="LOG OUT" />
+        <Header />
         <Dashboard stocks={this.ownedStocks()}/>
         <StockContainer stocks={this.ownedStocks()} stockType="OWNED" />
         <StockContainer stocks={this.watchedStocks()} stockType="WATCHED" />
@@ -83,7 +39,8 @@ const mapStateToProps = state => {
 
 const mapDispatchToProps = dispatch => {
   return({
-      fetchStock: (tickerSymbol, companyName, amountOfShares, costPerShare, id) => dispatch(fetchStock(tickerSymbol, companyName, amountOfShares, costPerShare, id))
+      fetchStock: (tickerSymbol, companyName, amountOfShares, costPerShare, id) => dispatch(fetchStock(tickerSymbol, companyName, amountOfShares, costPerShare, id)),
+      logoutUser: () => dispatch(logoutUser())
   })
 }
 
